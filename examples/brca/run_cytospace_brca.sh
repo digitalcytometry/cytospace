5fd2d0cbea0e718e086f4affa63665d0a0cabaa7
Chloé B. Steen <chloebs@stanford.edu>
2022-03-22 22:05:22 +0100

    Remove example files from repo

--- examples/brca/run_cytospace_brca.sh
+++
@@ -1,3 +0,0 @@
-Rscript /path/to/get_cellfracs_seuratv3.R brca_scRNA_GEP.txt brca_scRNA_celllabels.txt brca_STdata_slide1_GEP.txt brca_cell_fraction_estimates.txt
-
-cytospace -sp brca_scRNA_GEP.txt -ctp brca_scRNA_celllabels.txt -stp brca_STdata_slide1_GEP.txt -cp brca_STdata_slide1_coordinates.txt -ctfep brca_cell_fraction_estimates.txt
--- examples/melanoma/run_cytospace_melanoma.sh
+++
@@ -1,3 +0,0 @@
-Rscript ../../get_cellfracs_seuratv3.R melanoma_scRNA_GEP.txt melanoma_scRNA_celllabels.txt melanoma_STdata_slide1_GEP.txt melanoma_cell_fraction_estimates.txt
-
-cytospace -sp melanoma_scRNA_GEP.txt -ctp melanoma_scRNA_celllabels.txt -stp melanoma_STdata_slide1_GEP.txt -cp melanoma_STdata_slide1_coordinates.txt -ctfep melanoma_cell_fraction_estimates.txt
