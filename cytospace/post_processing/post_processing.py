95b95029d437de344d2ecc5db47e56ef605f12ec
Milad R. Vahid <82072965+miladrafiee@users.noreply.github.com>
2022-04-29 21:00:03 -0700

    Add place-holder approach

--- cytospace/post_processing/post_processing.py
+++ cytospace/post_processing/post_processing.py
@@ -7,7 +7,7 @@ from scipy.spatial.transform import Rotation
 from cytospace.common import read_file
 
 
-def save_results(output_path, output_prefix, cell_ids_selected, assigned_locations,
+def save_results(output_path, output_prefix, cell_ids_selected, cell_ids_new, assigned_locations,
                  new_cell_index, index, assigned_nodes, st_path, coords_path,
                  cell_type_path, assigned_locations_path):
 
@@ -21,7 +21,7 @@ def save_results(output_path, output_prefix, cell_ids_selected, assigned_locatio
         additional_columns = coords_columns[3:]
 
     cell_ids_selected_list = cell_ids_selected.tolist()
-
+    cell_ids_new_list = cell_ids_new.tolist()
     assigned_node_names = [st_names[l] for l in index]
 
     file_delim = "," if cell_type_path.endswith(".csv") else "\t"
@@ -32,6 +32,7 @@ def save_results(output_path, output_prefix, cell_ids_selected, assigned_locatio
     unique_cids = ['UCID'+str(i).zfill(numzeros) for i in range(len(cell_ids_selected_list))]
 
     df_locations = pd.DataFrame.from_dict({'UniqueCID': unique_cids,'OriginalCID': cell_ids_selected_list,
+                                'PlaceHolderCID': cell_ids_new_list,
                                 'CellType': list(df_labels.loc[cell_ids_selected_list,:][df_labels.columns[1]]),
                                 'SpotID': assigned_node_names,
                                 'row': list(assigned_locations.iloc[:, 0]),
@@ -55,3 +56,4 @@ def save_results(output_path, output_prefix, cell_ids_selected, assigned_locatio
     df_fracs = df.div(total_cells,axis=0)
     fout = output_path / f'{output_prefix}fractional_abundances_by_spot.csv'
     df_fracs.to_csv(fout)
+
