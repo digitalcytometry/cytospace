fc845acc121ddbb5e0d2799f0bf2aec4b0f60fd0
Hyun Soo Jeon <hsjeon@stanford.edu>
2023-06-16 18:25:54 -0700

    version update v1.0.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='cytospace',
-    version='1.0.5',
+    version='1.0.6',
     packages=['cytospace',
               'cytospace.common',
               'cytospace.linear_assignment_solvers',
